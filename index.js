c34444e3e8b3c064bbfc28ce7c487aa7354b947b
Pardis Rezaei <pardisrezaei@gmail.com>
2019-01-18 01:25:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Code your solution in this file!
 const startingPoint = 42
 
-//43 -> 1
+// 43 -> 1
 function distanceFromHqInBlocks(block) {
- 
+  if (block < startingPoint) {
+    return startingPoint - block
+  } else {
+    return block - startingPoint
+  }
+}
